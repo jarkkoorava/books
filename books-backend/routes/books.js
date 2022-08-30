66678be92f7758280faf6e2543c6a3bfcb9da481
Jarkko Orava <jarkko.orava@gmail.com>
2022-08-30 21:36:35 +0300

    Error handling to endpoints

--- books-backend/routes/books.js
+++ books-backend/routes/books.js
@@ -4,44 +4,57 @@ var router = express.Router();
 const { Book } = require('../models');
 
 router.get('/', async function (req, res, next) {
-  const books = await Book.findAll({ raw: false, order: [['id', 'ASC']] });
-  res.header('Access-Control-Allow-Origin', '*');
-  res.json(books);
+  try {
+    const books = await Book.findAll({ raw: false, order: [['id', 'ASC']] });
+    res.json(books);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/:id', async function (req, res, next) {
-  const book = await Book.findOne({ where: { id: req.params.id } });
-  res.header('Access-Control-Allow-Origin', '*');
-  res.json(book);
+  try {
+    const book = await Book.findOne({ where: { id: req.params.id } });
+    res.json(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async function (req, res, next) {
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-  console.log(req.body);
-  await Book.create({
-    title: req.body.title,
-    author: req.body.author,
-    description: req.body.description,
-  });
+  try {
+    await Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      description: req.body.description,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put('/:id', async function (req, res, next) {
-  const book = await Book.findOne({ where: { id: req.params.id } });
-  await book.update({
-    title: req.body.title,
-    author: req.body.author,
-    description: req.body.description,
-  });
-  res.sendStatus(200);
+  try {
+    const book = await Book.findOne({ where: { id: req.params.id } });
+    await book.update({
+      title: req.body.title,
+      author: req.body.author,
+      description: req.body.description,
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete('/:id', async function (req, res, next) {
-  const deleted = await Book.destroy({ where: { id: req.params.id } });
-  console.log(`Deleted ${deleted} rows`);
-  res.sendStatus(200);
+  try {
+    const deleted = await Book.destroy({ where: { id: req.params.id } });
+    console.log(`Deleted ${deleted} rows`);
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
